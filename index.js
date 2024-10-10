d7e68398f1ae2dbf558c1eadb52faf4f672f85d4
Chaitanya Hazarika <chaitanyahazarika@Ananyas-MacBook-Air.local>
2024-10-10 23:56:55 +0530

    some changes

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+
+
 app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('*', (req,res)=>{
-    const filePath = path.join(__dirname, 'build', 'index.html')
-    res.sendFile(filePath);
-})
-
 
 
 app.listen(4000);
